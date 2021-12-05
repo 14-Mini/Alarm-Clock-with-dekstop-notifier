1b559941d855a232640c0073b9f887a6acceba68
miniiiii <59201085+14-Mini@users.noreply.github.com>
2021-12-05 19:53:08 +0545

    Update AlarmClock.py
    added voice line.

--- AlarmClock.py
+++ AlarmClock.py
@@ -2,22 +2,33 @@ import os
 import datetime 
 from plyer import notification
 import time
+import pyttsx3
+
+def voice(msg):
+    var = pyttsx3.init()
+    var.say(msg)
+    var.runAndWait()
+
 
 os.system('CLS')
 alarm_Hour = int(input("Hour\n-"))
 alarm_Min = int(input("Minute\n-"))
 alarm_AmOrPm = str(input("AM or PM\n-"))
+get_message = str(input("What do you want your message to be? \n-"))
 time.sleep(1)
-print("You have set the alarm for", alarm_Hour,":",alarm_Min,alarm_AmOrPm.upper())
+print("\nYou have set the alarm for", alarm_Hour,":",alarm_Min,alarm_AmOrPm.upper())
 time.sleep(0.5)
-print("Waitng for alarm to ring!")
+print("="*45)
+print("\nWaitng for alarm to ring!")
 time.sleep(1)
-print("You will be notified by the desktop notifier about the alarm.")
+print("="*45)
+print("You will be notified by the desktop notifier about the alarm and you should be hearing a voice message.")
 
 if (alarm_AmOrPm.upper() == "PM"):
     alarm_Hour = alarm_Hour + 12
 
 while True:
     if (alarm_Hour == datetime.datetime.now().hour and alarm_Min == datetime.datetime.now().minute):
-        notification.notify(title = 'ALARM', message = 'MESSAGE HERE', timeout = 10)
+        notification.notify(title = 'ALARM', message = (get_message), timeout = 10)
+        voice(get_message)
         break
